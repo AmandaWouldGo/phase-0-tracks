df88112d058f50ff2978011f12a8aaf35643eede
AmandaWouldGo <amandawouldgo@gmail.com>
2016-05-23 18:36:35 -0700

    Add initial info gathering questions.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name? "
+name = gets
+
+puts "How old are you? "
+age = gets
+
+puts "What year were you born? "
+birth_year = gets
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? "
+garlic_bread = gets
+
+puts "Would you like to enroll in the company’s health insurance? "
+insurance = gets
