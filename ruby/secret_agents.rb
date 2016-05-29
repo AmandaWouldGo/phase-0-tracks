1319f92ec722c8a30c47e6a2b3a6df29c1c44845
AmandaWouldGo <amandawouldgo@gmail.com>
2016-05-29 12:42:24 -0700

    Add user interface to run program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,7 +1,6 @@
 # Pseudo code for method 1
 # Ask user for a string
 # Transform string, move all letters one forward in alphabet
-
 def encrypt(password)
   index = 0
   while index < password.length
@@ -34,10 +33,30 @@ def decrypt(password)
   index += 1
   end
  p password
-  end
+end
+ 
+def interface 
+#Ask agent if they want to encrypt or decrypt a password
+puts "Would you like to decrypt or encrypt a password?"
+choice = gets.chomp.downcase
+#Ask agent for password
+puts "Type password:"
+input = gets.chomp
+#Run relevant program, print results
+if choice == "decrypt"
+  decrypt(input)
+elsif choice == "encrypt"
+  encrypt(input)
+else puts "You are not a secret agent. Goodbye"
+end
+puts "goodbye"
+end
 
-encrypt("abc")
-encrypt("zed")
-decrypt("bcd")
-decrypt("afe")
-decrypt(encrypt("swordfish"))
+interface
+
+#encrypt("abc")
+#encrypt("zed")
+#decrypt("bcd")
+#decrypt("afe")
+
+#decrypt(encrypt("swordfish"))
