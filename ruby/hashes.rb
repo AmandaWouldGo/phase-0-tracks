3dc8e611b81c5f4e1456d02dca64fddf21315c50
AmandaWouldGo <amandawouldgo@gmail.com>
2016-05-30 19:05:16 -0700

    Initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,30 @@
+#this program creates a hash of interior design client data
+#user: interior designer
+#user enters client data: name, age, # of children. decor theme, etc. 
+
+#prompt user for client data and store answers
+#create a method that holds my program
+def interior_design
+puts "Enter your design client data"
+puts "Name:"
+
+puts "Age:"
+
+puts "Number of children:"
+
+puts "Decor theme:"
+
+puts "Has a fireplace:"
+#convert user input to appropriate data type
+#print has to screen when user has answered all q's
+
+#let user update key by entering the key name
+#prompt for specific key value
+#if user answers anything other than none, the key should update with new value
+#print latest version of hash
+#exit program
+
+end
+
+#call method interior_design
+interior_design
