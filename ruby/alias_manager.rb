5fd7cd75338d46626a293bee61486e79d3ada944
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-02 21:49:27 -0700

    update variables, .split .reverse .chars

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,11 +4,25 @@
 
 #gets input from user
 puts "Welcome, spy, enter your name and I will make you a super secret new name:"
-#swap first and last name
 real_name = gets.chomp
-new_name = real_name.split(' ')
-#change all vowels to the next vowel (.next)?
+#swap first and last name
+new_name = real_name.split(' ').reverse
+	#test print (p new_name => ["Cook", "Amanda"])
+
+#iterate through array, select each word
+
+first_name = new_name[0] 
+last_name = new_name[1] 
+
+	#(p first_name prints "Cook" and p last_name prints "Amanda")
+
+#split each word into it's individual letters
+
+first_name = first_name.chars
+last_name = last_name.chars
+
+	#p first_name ["C", "o", "o", "k"] and p last_name ["A", "m", "a", "n", "d", "a"]
+
+#advance vowel to next vowel
 
 #write separate next_vowel method to combine with other methods...
-#def next_vowel
-#new_name.
