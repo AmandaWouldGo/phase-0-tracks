44ed76eb60f6ad6ac3fa2b96c9f3115ea9c34160
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-02 21:25:55 -0700

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+#This code will generate a code name for a spy when the spy's name is entered.
+
+#define a method to hold my spy name program
+
+#gets input from user
+puts "Welcome, spy, enter your name and I will make you a super secret new name:"
+#swap first and last name
+real_name = gets.chomp
+new_name = real_name.split(' ')
+#change all vowels to the next vowel (.next)?
+
+#write separate next_vowel method to combine with other methods...
+#def next_vowel
+#new_name.
