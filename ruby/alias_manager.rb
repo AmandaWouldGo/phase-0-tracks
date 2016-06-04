102fd41ad135d0c03036a731d2a3167d2adf4dcd
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-04 16:04:15 -0700

    Add second method that allows user to type in names until they type 'quit'.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,78 +2,96 @@
 ### coder acknowledges that this IS NOT DRY!! (LOL) and will find a better way, hopefully sometime soonish!!###
 #define a method to hold my spy name program
 
-#gets input from user
 puts "Welcome, spy, enter your first and last name and I will make you a super secret new name:"
-real_name = gets.chomp.downcase
-#swap first and last name
-new_name = real_name.split(' ').reverse
-	#test print (p new_name => ["Cook", "Amanda"])
 
-#separate each word, into own array of letters
-
-first_name = new_name[0].chars
-last_name = new_name[1].chars
-	#p first_name ["c", "o", "o", "k"] and p last_name ["a", "m", "a", "n", "d", "a"]
-
-#write block with not-DRY if statement to check for conditions at iterates through first_name and last_name separately
-first_name.map! do |letter|
-	if letter == "a"
-		letter = "e"
-	elsif letter == "e"
-		letter = "i"
-	elsif letter == "i"
-		letter = "o"
-	elsif letter == "o"
-		letter = "u"
-	elsif letter == "u"
-		letter = "a"
-	elsif letter == "z"
-		letter = "b"
-	elsif letter == "d"
-		letter = "f"
-	elsif letter == "h"
-		letter = "j"
-	elsif letter == "n"
-		letter = "p"
-	elsif letter == "t"
-		letter = "v"
-	else letter.next
+def name_code
+	#gets input from user
+	#add method that starts name_code over
+	def try_again
+		puts "If you'd like to try another name press enter, if not, type quit"
+		input = gets.chomp.downcase
+		if input == "quit"
+		  puts "Thank you, goodbye spy"
+		else 
+			puts "Type another name:"
+			name_code
+		end
 	end
+
+	real_name = gets.chomp.downcase
+	#swap first and last name
+	new_name = real_name.split(' ').reverse
+		#test print (p new_name => ["Cook", "Amanda"])
+
+	#separate each word, into own array of letters
+
+	first_name = new_name[0].chars
+	last_name = new_name[1].chars
+		#p first_name ["c", "o", "o", "k"] and p last_name ["a", "m", "a", "n", "d", "a"]
+
+	#write block with not-DRY if statement to check for conditions at iterates through first_name and last_name separately
+	first_name.map! do |letter|
+		if letter == "a"
+			letter = "e"
+		elsif letter == "e"
+			letter = "i"
+		elsif letter == "i"
+			letter = "o"
+		elsif letter == "o"
+			letter = "u"
+		elsif letter == "u"
+			letter = "a"
+		elsif letter == "z"
+			letter = "b"
+		elsif letter == "d"
+			letter = "f"
+		elsif letter == "h"
+			letter = "j"
+		elsif letter == "n"
+			letter = "p"
+		elsif letter == "t"
+			letter = "v"
+		else letter.next
+		end
+	end
+
+	last_name.map! do |letter|
+		if letter == "a"
+			letter = "e"
+		elsif letter == "e"
+			letter = "i"
+		elsif letter == "i"
+			letter = "o"
+		elsif letter == "o"
+			letter = "u"
+		elsif letter == "u"
+			letter = "a"
+		elsif letter == "z"
+			letter = "b"
+		elsif letter == "d"
+			letter = "f"
+		elsif letter == "h"
+			letter = "j"
+		elsif letter == "n"
+			letter = "p"
+		elsif letter == "t"
+			letter = "v"
+		else letter.next
+		end
+	end
+
+	#use .join to make string from array and capitalize it, because it's a name.
+	first_name = first_name.join.capitalize!
+	last_name = last_name.join.capitalize!
+
+	#p first_name
+	#p last_name
+
+	#use join to bring whole code_name together into one masterpiece
+	code_name = first_name + ' ' + last_name
+	p "The world will now refer to you as: #{code_name}"
+	try_again
+
 end
 
-last_name.map! do |letter|
-	if letter == "a"
-		letter = "e"
-	elsif letter == "e"
-		letter = "i"
-	elsif letter == "i"
-		letter = "o"
-	elsif letter == "o"
-		letter = "u"
-	elsif letter == "u"
-		letter = "a"
-	elsif letter == "z"
-		letter = "b"
-	elsif letter == "d"
-		letter = "f"
-	elsif letter == "h"
-		letter = "j"
-	elsif letter == "n"
-		letter = "p"
-	elsif letter == "t"
-		letter = "v"
-	else letter.next
-	end
-end
-
-#use .join to make string from array and capitalize it, because it's a name.
-first_name = first_name.join.capitalize!
-last_name = last_name.join.capitalize!
-
-#p first_name
-#p last_name
-
-#use join to bring whole code_name together into one masterpiece
-code_name = first_name + ' ' + last_name
-p code_name
-
+name_code
