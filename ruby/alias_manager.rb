5ba7a078cc54f46109acdc5ed6b151d5aaa3c931
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-04 15:39:15 -0700

    Use long if statement in block as work around for vowel and consonant advancement. Understand there is a more DRY way to do so. Will attempt DRY.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,10 @@
 #This code will generate a code name for a spy when the spy's name is entered.
-
+### coder acknowledges that this IS NOT DRY!! (LOL) and will find a better way, hopefully sometime soonish!!###
 #define a method to hold my spy name program
 
 #gets input from user
 puts "Welcome, spy, enter your first and last name and I will make you a super secret new name:"
-real_name = gets.chomp
+real_name = gets.chomp.downcase
 #swap first and last name
 new_name = real_name.split(' ').reverse
 	#test print (p new_name => ["Cook", "Amanda"])
@@ -14,19 +14,59 @@ new_name = real_name.split(' ').reverse
 first_name = new_name[0].chars
 last_name = new_name[1].chars
 	#p first_name ["c", "o", "o", "k"] and p last_name ["a", "m", "a", "n", "d", "a"]
-#declare variable for vowels (see secret_agents.rb for more info)
-
-#vowels = "aeiou"
 
 first_name.map! do |letter|
-	letter.next
+	if letter == "a"
+		letter = "e"
+	elsif letter == "e"
+		letter = "i"
+	elsif letter == "i"
+		letter = "o"
+	elsif letter == "o"
+		letter = "u"
+	elsif letter == "u"
+		letter = "a"
+	elsif letter == "z"
+		letter = "b"
+	elsif letter == "d"
+		letter = "f"
+	elsif letter == "h"
+		letter = "j"
+	elsif letter == "n"
+		letter = "p"
+	elsif letter == "t"
+		letter = "v"
+	else letter.next
+	end
 end
 
 last_name.map! do |letter|
-	letter.next
+	if letter == "a"
+		letter = "e"
+	elsif letter == "e"
+		letter = "i"
+	elsif letter == "i"
+		letter = "o"
+	elsif letter == "o"
+		letter = "u"
+	elsif letter == "u"
+		letter = "a"
+	elsif letter == "z"
+		letter = "b"
+	elsif letter == "d"
+		letter = "f"
+	elsif letter == "h"
+		letter = "j"
+	elsif letter == "n"
+		letter = "p"
+	elsif letter == "t"
+		letter = "v"
+	else letter.next
+	end
 end
 
-first_name = first_name.join
+#first_name = first_name.join.capitalize!
+#last_name = last_name.join.capitalize!
 
 p first_name
 p last_name
