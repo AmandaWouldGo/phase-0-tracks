60694cd1fce14f4d95baf14c30b414f34e95c2f4
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-04 14:53:35 -0700

    Add correctly functioning letter iteration to first name.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,25 +3,26 @@
 #define a method to hold my spy name program
 
 #gets input from user
-puts "Welcome, spy, enter your name and I will make you a super secret new name:"
+puts "Welcome, spy, enter your first and last name and I will make you a super secret new name:"
 real_name = gets.chomp
 #swap first and last name
 new_name = real_name.split(' ').reverse
 	#test print (p new_name => ["Cook", "Amanda"])
 
-#iterate through array, select each word
+#separate each word, into own array of letters
 
-first_name = new_name[0] 
-last_name = new_name[1] 
+first_name = new_name[0].chars
+last_name = new_name[1].chars
+	#p first_name ["c", "o", "o", "k"] and p last_name ["a", "m", "a", "n", "d", "a"]
+#declare variable for vowels (see secret_agents.rb for more info)
 
-	#(p first_name prints "Cook" and p last_name prints "Amanda")
+#vowels = "aeiou"
 
-#split each word into it's individual letters
+first_name.map! do |letter|
+	letter.next
+end
 
-first_name = first_name.chars
-last_name = last_name.chars
-
-	#p first_name ["C", "o", "o", "k"] and p last_name ["A", "m", "a", "n", "d", "a"]
+p first_name
 
 #advance vowel to next vowel
 
