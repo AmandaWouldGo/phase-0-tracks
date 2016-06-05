d7a67ba81086948b137dc278cfc3c0b2c79e33e0
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-04 22:09:49 -0700

    Initial commit

---
+++ ruby/house_maker.rb
@@ -0,0 +1,34 @@
+#HOUSE MAKER
+
+#Allow the user to create a house,
+#then add rooms,
+#then add items
+
+#House can have up to 5 rooms
+#Room can have an unlimited number of items
+
+#BUSINESS LOGIC
+
+def add_room_to_house!(house, room_name)
+	house[room_name] = []
+end
+
+house = {}
+
+#USER INTERFACE
+
+def print_house(house)
+	p house
+end
+
+#TEST CODE
+
+rooms = ["Living room", "bedroom", "bathroom", "kitchen"]
+
+rooms.each do |room|
+	add_room_to_house(house, room)
+end
+
+print_house(house)
+
+#USER INTERFACE
