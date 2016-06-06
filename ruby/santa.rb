c32cc931eb452bfd25b53b4c9783fe92ba8b7f6e
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-06 11:03:30 -0700

    Add first methods and test them.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,2 +1,17 @@
-class = Santa
+class Santa
+	def initialize
+		p "Initializing Santa instance..."
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaapy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		p "This was a good #{cookie_type}!"
+	end
+
+	bah_humbug = Santa.new
+	bah_humbug.speak
+	bah_humbug.eat_milk_and_cookies("triple chocolate")
 end
