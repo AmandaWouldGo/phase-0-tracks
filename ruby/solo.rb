c369dc9e631146a7290e6297533054c324d8ea79
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-09 08:14:53 -0700

    Add Muppet class design.

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+#CLASS ------------------------
+#Muppets
+
+#CHARACTERISTICS ----------------
+#Color: varies
+#Hair: varies
+#Eye_size: varies
+#Name: varies
+#Clothing: varies
+
+#BEHAVIOR -----------------------
+#Make friends
+#Tell jokes
+#Sing
