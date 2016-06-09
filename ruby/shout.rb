a2fa7420e07bf565f2d9e86ea28864a9c5513d7e
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-09 13:12:08 -0700

    Add shout module, add methods to module, add driver code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,1 +1,14 @@
- 
+module Shout
+  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    puts words + "!!!" + " :)"
+    "Woohooooo!"
+  end
+end
+
+p Shout.yell_angrily(";alsdjl;as df;k")
+p Shout.yelling_happily("I did it")
