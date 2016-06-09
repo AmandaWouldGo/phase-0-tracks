bac3ff866be74af3b63741d71912d71a39f8ac6e
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-09 13:21:40 -0700

    Comment out orig Shout module and add mixin Shout module, methods and driver code.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,41 @@
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     puts words + "!!!" + " :)"
+#     "Woohooooo!"
+#   end
+# end
+
+# p Shout.yell_angrily(";alsdjl;as df;k")
+# p Shout.yelling_happily("I did it")
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     puts words + "!!!" + " :)"
     "Woohooooo!"
   end
 end
 
-p Shout.yell_angrily(";alsdjl;as df;k")
-p Shout.yelling_happily("I did it")
+class Baby
+  include Shout
+end
+
+class Cheerleader
+  include Shout
+end
+
+baby = Baby.new
+baby.yelling_happily("googoo")
+baby.yell_angrily("mama")
+
+cheerleader = Cheerleader.new
+cheerleader.yelling_happily("Go team")
+cheerleader.yell_angrily("You stole my boyfriend you b@*!%")
