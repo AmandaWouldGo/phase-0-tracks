5547541990cd188993de61ad7b4d7333953c37a4
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-13 10:55:15 -0700

    Explore js, add functions that test the steps to make my function work.

--- js/explore.js
+++ js/explore.js
@@ -3,7 +3,37 @@
 // input: string
 // what happens: 
 // - enter a word as an argument in the function.
-// - function loops over string and reads each character individually
+// ---function must have take a parameter
+// - function loops over string and reads each character individually.
+// ---This uses a for loop
 // - function returns each character in reverse order
 // - function adds each character to put back together as string
 // output: string reversed
+
+// function reverseString(str) {
+// 	console.log(str);
+// }
+
+var muppet = "muppet";
+
+// reverseString(muppet);
+
+// function reverseString(str) {
+// 	for (c = 0; c < str.length; c++) {
+// 		console.log(str[c]);
+// 	}
+// }
+
+function reverseString(str) {
+	for (c = str.length - 1; c >= 0; c--) {
+		console.log(str[c]);
+	}
+}
+
+function reverseString(str) {
+	for (c = str.length - 1; c >= 0; c--) {
+		console.log(str[c]);
+	}
+}
+
+reverseString(muppet);
