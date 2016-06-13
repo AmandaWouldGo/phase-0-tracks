b3f1b08ce954b754c15249b8d4117d2700a67963
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-13 10:27:29 -0700

    Pseudocode js function for reversing a word.

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,9 @@
 // Here we go!
+// Function: reverse a string
+// input: string
+// what happens: 
+// - enter a word as an argument in the function.
+// - function loops over string and reads each character individually
+// - function returns each character in reverse order
+// - function adds each character to put back together as string
+// output: string reversed
