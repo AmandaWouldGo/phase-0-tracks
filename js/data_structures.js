bc405d04ad4b104789437b89d140e93c0440e676
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-14 14:29:41 -0700

    Add names and colors from arrays to object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,5 +6,17 @@
 
  names.push("Jim");
 
- console.log(colors);
- console.log(names);
+ // console.log(colors);
+ // console.log(names);
+
+ var horse = {};
+
+ // set up for loop and assign two values of the arrays to the object
+ // reminder use +=
+
+ for (var i = 0; i < names.length; i++) {
+ 	horse[names[i]] = colors[i];
+ }
+
+ console.log(horse);
+
