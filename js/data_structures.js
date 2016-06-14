18fce42f45768371e56573558536ec3412f5c813
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-14 14:18:13 -0700

    Add two arrays and push new items to arrays.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,1 +1,10 @@
- 
+ var colors = ["orange", "blue", "pink", "red"];
+
+ var names = ["The dude", "Mr. Ed", "Speedracer", "Exaggerated"];
+
+ colors.push("greenish");
+
+ names.push("Jim");
+
+ console.log(colors);
+ console.log(names);
