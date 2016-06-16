6ada174c269873931a1e2ec81b111ac26e2689a7
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-16 12:14:52 -0700

    Initial commit

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+//write a function that takes an array of strings
+//iterate over array to find the longest string
+//return the longest string
+
+testArray = ["tiny", "small", "longer"]
+
+function longestString(array) {
+	for (i = 0; i < 5; i++) {
+		console.log(i.length);
+	}
+}
+
+longestString(testArray);
