a83a2aa5123dbafbacf9224de7b4ec7e2ab1425e
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-16 17:49:28 -0700

    Add Lauren's additional refactoring code and my reflection.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Lauren].
+# We spent 1.5 hours on this challenge.
 
 # EXPLANATION OF require_relative
 # Require_relative accesses another file (state_data.rb) that is holding info
@@ -20,34 +20,34 @@ class VirusPredictor
 
 # runs the two private methods within the class
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
 # directly calls the attributes on the instance of the class, performs logic with them
 # output: a final summary string
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = (@population * 0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      number_of_deaths = (@population * 0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      number_of_deaths = (@population * 0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      number_of_deaths = (@population * 0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = (@population * 0.05)
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{number_of_deaths.floor} people in this outbreak"
 
   end
 
 # sets a variable value to zero but reassigns it based on the results of the conditional.
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -86,7 +86,7 @@ end
 #=======================================================================
 
 # DRIVER CODE
- # initialize VirusPredictor for each state
+# initialize VirusPredictor for each state
 # iterate through the state_data hash to build instances of VirusPredictor class for each state
 
 VirusPredictor.state_report
@@ -107,3 +107,10 @@ VirusPredictor.state_report
 
 #=======================================================================
 # Reflection Section
+
+# The differences between the hash syntaxes in state_data were that one used hash rockets and the other used symbols.
+# require_relative accesses a file within the same directory. Require can access a file from anywhere.
+# Are you asking for methods to iterate through hashes? .each and block, .map and block, a for loop (although I haven't used one yet in ruby)
+# In virus_effects all of the arguements were redundant. They could be accessed directly from initialize. Therefore, we removed them
+#   from both virus_effects as well as predicted_deaths and speed_of_spread. 
+# I solidified the use of self AND the use of the method private AND the use of a constant. All of these were new to me, but made sense as we used them!
