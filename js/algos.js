898e7a702e5123bcaa75dd78566df17c63516726
AmandaWouldGo <amandawouldgo@gmail.com>
2016-06-18 16:03:56 -0700

    Write and test longestString function.

--- js/algos.js
+++ js/algos.js
@@ -3,13 +3,31 @@
 //move longest string at front of array at index 0
 //print index 0 to return the longest string
 
-testArray = ["tiny", "small", "longer"]
+testArray = ["tiny", "small", "longer"];
+testArray2 = ["Molly", "Marty", "Amanda"];
+testArray3 = ["Orange is the New Black", "Love", "Unbreakable Kimmie Schmidt", "Master of None"]
+longest = "";
 
 function longestString(array) {
 	
 	for (i = 0, len = array.length; i < len; i++) {
-		console.log(array[i].length)
+		if (longest.length <= array[i].length) {
+			longest = array[i];
+		}
 	}
+
+	console.log(longest);
 }
 
-longestString(testArray);
+// longestString(testArray);
+// longestString(testArray2);
+// longestString(testArray3);
+
+// shared_value function
+
+function shared_value(object1, object2) {
+	
+}
+
+
+
